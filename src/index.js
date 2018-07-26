e305532f9bef4545f15c2824277008e3ff72742a
wesley.wang <wesley.wang@zerologix.com>
2018-07-27 09:29:51 +1000

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import antd from 'antd';
+const Row = antd.Row;
+const Col = antd.Col;
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
@@ -15,7 +17,10 @@ export default class ExampleComponent extends Component {
 
     return (
       <div className={styles.test}>
-        Example Component: {text}
+        <Row>
+          <Col span={18} push={6}>col-18 col-push-6</Col>
+          <Col span={6} pull={18}>col-6 col-pull-18</Col>
+        </Row>
       </div>
     )
   }
