aa656f60fd2e5675400b0387c71992aaea265b62
wesley.wang <wesley.wang@zerologix.com>
2018-07-26 17:41:23 +1000

    rollup sample

--- src/lib/Button/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-// const Button = ({ text }) => <button>{text}</button>;
-const Button = function({children}){
-  return <button>{children}</button>;
-}
-export default Button;
--- src/lib/index.js
+++
@@ -1,5 +0,0 @@
-import Button from './Button/index';
-
-module.exports = {
-  Button,
-};
