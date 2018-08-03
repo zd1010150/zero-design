59183586c1d0969b6f70c830f1670e2bda22a4f7
wesley.wang <wesley.wang@zerologix.com>
2018-08-03 12:17:07 +1000

    minor change

--- src/lib/filter/component/brandList.js
+++ src/lib/filter/component/brandList.js
@@ -6,8 +6,8 @@ import ActionRow from './actionRow';
 const BrandRow = styled.div`
   transition: max-height 100ms ease-in-out, margin-top 100ms ease-in-out;
   overflow: hidden;
-  max-height: ${props => (props.collapse ? "85px" : "1500px")};
-  margin-top: ${props => (props.collapse ? "-40px" : 0)};
+  max-height: ${props => (props.collapse ? "80px" : "1500px")};
+  margin-top: ${props => (props.collapse ? "-35px" : 0)};
 `;
 
 const StyledCheckBox = styled.span`
